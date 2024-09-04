422e256063352bfe812e4d21e2306cb76aa9eb78
albajserra <a.joaniquet.serra@student.rug.nl>
2024-09-04 16:25:36 +0200

    finalised version

--- r-scripts/06-relational-database-joins.R
+++ r-scripts/06-relational-database-joins.R
@@ -44,27 +44,55 @@ cdat2017 <- read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSpormjGiM
                    sd_l=sd(length_mm,na.rm = T), # calculate standard deviation
                    se_l=sd_l/sqrt(n_obs)) # calculate standard error
 print(cdat2017)
+
 # plot (with a line and points)  how the number of cockles changes with distance along the transect
-
-
 ##### merge the cockle and elevation data into a single table you call "combidat"
-# using Distance_ID as the common variable between the two tables
-
+# using TransectPoint_ID as the common variable between the two tables
+combidat <- dplyr::left_join(elevdat2017,cdat2017,by="TransectPoint_ID") |>
+  dplyr::mutate(n_obs=tidyr::replace_na(n_obs,0))
+combidat
 # show in a plot how cockle density changes with elevation
-
-# fit a linear regression
+combidat |>
+  ggplot(aes(x=elevation_m,y=n_obs)) + 
+  geom_point() + 
+  geom_smooth(method="loess") # fit a linear regression
 
 # predicted at 0.5 m (x)
-# y = b0 + b1x   (b0 is intercept and b1 is the slope, x is elevation, y is no cockles
-
+# y = b0 + b1x   (b0 is intercept and b1 is the slope, x is elevation, y is no cockles)
+model_lm<-lm(n_obs~elevation_m, data=combidat)
+summary(model_lm)
+1.36820 -0.01396*0.5
 # show this model as a line in ggplot, with the confidence interval
-
+ggplot2::ggplot(data=combidat,
+                mapping=aes(x=elevation_m,y=n_obs)) + 
+  geom_point() + 
+  geom_smooth(method="lm")
 # fit a better model, using a loess smoother
+ggplot2::ggplot(data=combidat,
+                mapping=aes(x=elevation_m,y=n_obs)) + 
+  geom_point() + 
+  geom_smooth(method="loess")
 # show this model in ggplot
 
-##### plot  how the size (as mean length) of cockles changes with  elevation along the transect
+##### plot how the size (as mean length) of cockles changes with elevation along the transect
 # omit the observations where length is NA (because no cockles were measures)
 # fit a quadratic model (second-order polynomial)
 # show for each point also the standard errors
 # add appropriate labels for the x and y axis 
+combidat_size <- combidat |>
+  dplyr::mutate(n_obs=tidyr::replace_na(n_obs,0)) |>
+  dplyr::filter(avg_l!=0)
 
+ggplot(data=combidat_size,
+                mapping=aes(x=elevation_m,y=avg_l)) + 
+  geom_point() + 
+  xlab("elevation (mm)") + #lab = labels
+  ylab("cockle length (mm)") + 
+  geom_smooth(method="loess")
+
+#create a new variable for combidat_size
+combidat_size$elevation_m2 <- combidat_size$elevation_m^2
+
+#fit quadratic regression model
+quadraticModel <- lm(avg_l ~ elevation_m + elevation_m2, data=combidat_size)
+summary(quadraticModel)
