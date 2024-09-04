be30646234e84c61cf87423273df9d9455e5fda8
albajserra <a.joaniquet.serra@student.rug.nl>
2024-09-04 14:48:11 +0200

    filtered data to a specific year and calculated mean, sd and se

--- r-scripts/06-relational-database-joins.R
+++ r-scripts/06-relational-database-joins.R
@@ -4,26 +4,46 @@
 # clear everything in memory
 rm(list=ls())
 
-# load libraries
+# restore and load libraries
+renv::restore()
 library(tidyverse) # including ggplot2, dplyr that we 
 
 # load the elevation data and show the first 10 records of the dataset
-elevdat<-read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vT4C7olgh28MHskOjCIQGYlY8v5z1sxza9XaCccwITnjoafF_1Ntfyl1g7ngQt4slnQlseWT6Fk3naB/pub?gid=1550309563&single=true&output=csv")
+names(elevdat)
+elevdat<-read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vT4C7olgh28MHskOjCIQGYlY8v5z1sxza9XaCccwITnjoafF_1Ntfyl1g7ngQt4slnQlseWT6Fk3naB/pub?gid=1550309563&single=true&output=csv") |>
+  dplyr::mutate(year=factor(year)) # make year a factor so it is not a continuous variable but a discrete category
 elevdat
 
 # plot the change in transect  elevation along the transect, using a separate graph for each for each year 
-
+elevdat |>
+  ggplot(aes(x=TransectPoint_ID, y=elevation_m)) + 
+  geom_line() + 
+  facet_wrap(~year) # each year is separated in a different graph
 # plot the change in transect  elevation along the transect, using a separate line color for each year 
-
+elevdat |>
+  ggplot(aes(x=TransectPoint_ID, y=elevation_m,col=year)) + 
+  geom_line()
 # Extract the data for 2017 in a new tibble, keep only variables distance_m and elevation
 # omit records where Distance_ID is missing (NA)
+elevdat2017 <- elevdat |>
+  dplyr::filter(year==2017) |> # select only the year 2017
+  dplyr::select(TransectPoint_ID, elevation_m) |> # only choose TransectPoint_ID and elevation_m as variables
+  dplyr::filter(!is.na(TransectPoint_ID)) # omit records where Distance_ID is missing
 
 # read the cockle data 
 # keep only the data for 2017, 
 # omit observations (Obs_ID) 468 and 1531
 # calculate the mean number of cockles and mean size for each distance
-cdat2017 <- read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSpormjGiM4uWMTIZYm9Upt5j1Ige_Wu9CrhGGdvjXwxmHP2R6S7JDyEmiGTn3fXihl5s5yBn_sdo8h/pub?gid=1538766002&single=true&output=csv") 
-
+cdat2017 <- read_csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vSpormjGiM4uWMTIZYm9Upt5j1Ige_Wu9CrhGGdvjXwxmHP2R6S7JDyEmiGTn3fXihl5s5yBn_sdo8h/pub?gid=1538766002&single=true&output=csv") |>
+  dplyr::filter(year==2017, # only use 2017
+                CockleObs_ID!=468,
+                CockleObs_ID!=1531) |> # remove outliers
+  dplyr::group_by(TransectPoint_ID) |> # group by distance
+  dplyr::summarize(n_obs=n(),
+                   avg_l=mean(length_mm,na.rm = T), # calculate mean
+                   sd_l=sd(length_mm,na.rm = T), # calculate standard deviation
+                   se_l=sd_l/sqrt(n_obs)) # calculate standard error
+print(cdat2017)
 # plot (with a line and points)  how the number of cockles changes with distance along the transect
 
 
