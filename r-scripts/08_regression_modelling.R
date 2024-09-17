c91bbb7d5fc79522880244f1d0caf33a57d7a5b5
albajserra <a.joaniquet.serra@student.rug.nl>
2024-09-17 10:36:01 +0200

    08 finished

--- r-scripts/08_regression_modelling.R
+++ r-scripts/08_regression_modelling.R
@@ -111,15 +111,15 @@ p1 + geom_line(data = orchdat3,aes(y = pred4,col=factor(year)),linewidth = 1.2)
 
 # test if this model is significantly better than the previous one
 anova(m4,m3) # put model you want to compare to old one
-#### m3 is the preferred model in this case (outcome of model selection, m4). The interaction term is not significant, so we prefer the simpler model m3
-
 # add the linear model to the plot
 # calculate the predicted value of m2 for every observation, add to the dataset as a variable as pred2
 # add the new predicted line to the previous plot p2, store as object p3 and show it
 
+#### m3 is the preferred model in this case (outcome of model selection, m4). The interaction term is not significant, so we prefer the simpler model m3
+
 # explore the consequences of a log transformation of y values
 x<-c(0,1,2,3,4,5)
-y<-c(1,10,100,1000,10000,100000)
+y<-c(0,0,80,400,10000,100000)
 dat<-data.frame(x,y)
 dat
 dat %>% ggplot(aes(x=x,y=y)) +
@@ -127,10 +127,11 @@ dat %>% ggplot(aes(x=x,y=y)) +
   geom_line(size=1.2)
 dat %>% ggplot(aes(x=x,y=log10(y))) +
   geom_point(shape=16,size=3) +
-  geom_line(size=1.2)
+#  geom_line(size=1.2)
+  geom_smooth(method="lm") # add a linear model to the plot
 dat %>% ggplot(aes(x=x,y=log(y))) + # use log with base number e
   geom_point(shape=16,size=3) +
-  geom_line(size=1.2)
+   geom_line(size=1.2)
 # explore the consequences of the log base number (10 or e)
 log(0)
 log10(1)
@@ -145,34 +146,54 @@ log(exp(1))
 # Explore  how the abundance of Orchestia depends on elevation_m and year,  their potential interaction,
 # and a potential ecological optimum of Orchestia with respect to elevation_m
 # show the effect of elevation but now in a generalized linear model instead of linear model, using a log link function and a poisson distribution
-
+orchdat3
+p1
 
 #add the linear model to the plot
 # calculate the predicted value of m2 for every observation, add to the dataset as a variable as pred2
 # add the new predicted line to the previous plot p2, store as object p3 and show it
-
-
-
+m5 <- glm(CountSum~elevation_m,
+         family = poisson(log),
+         data=orchdat3) # ~ means y explained by x. Linear model of CountSum as a function of elevation_m. Family means what do i assume for the distribution of the residuals -> assume poisson distribution
 # now test and show  the effect of both elevation , elevation squared and year
 
+anova(m5,test="Chisq") # test if the model is significant
+
 
 #add the linear model to the plot
 # calculate the predicted value of m2 for every observation, add to the dataset as a variable as pred2
 # add the new predicted line to the previous plot p2, store as object p3 and show it
+orchdat3$pred5<-predict(m5,type="response") # predict the values of m5
+p1 + geom_line(data = orchdat3,aes(y = pred5), linewidth = 1.2) # add the line to the plot
 
+#account for ecological optimum
+m6 <- glm(CountSum~elevation_m+I(elevation_m^2),
+          family = poisson(log),
+          data=orchdat3)
+orchdat3$pred6<-predict(m6,type="response") # predict the values of m6
+p1 + geom_line(data = orchdat3,aes(y = pred6), linewidth = 1.2)
 
-# better than the previous?
-
-
-# add the interaction to the model: elevation + elevation ^2 + year + elevation*year
+  # add the interaction to the model: elevation + elevation ^2 + year + elevation*year
 # now test and show  the effect of both elevation + year
+m7 <- glm(CountSum~elevation_m+I(elevation_m^2)+factor(year),
+         family = poisson(log),
+         data=orchdat3) # factor(year) means interpret year as a factor
+orchdat3$pred7<-predict(m7,type="response") # predict the values of m5
+p1 + geom_line(data = orchdat3,aes(y = pred7), linewidth = 1.2)
 
+anova(m7,m6, test="Chisq") # test if the model is significant
+
+m8 <- glm(CountSum~elevation_m+I(elevation_m^2)+factor(year) + 
+           elevation_m*factor(year),
+         family = poisson(log),
+         data=orchdat3)
+orchdat3$pred8<-predict(m8,type="response") # predict the values of m8
+p1 + geom_line(data = orchdat3,aes(y = pred8), linewidth = 1.2)
+
+anova(m8,m7, test="Chisq") # test if the model is significant
 
 #add the  model to the plot
 # calculate the predicted value of m2 for every observation, add to the dataset as a variable as pred2
 # add the new predicted line to the previous plot p2, store as object p3 and show it
-
-
-
 
 
